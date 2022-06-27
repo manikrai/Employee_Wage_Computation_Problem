c127508e1419d964b0df1d227c4a52809f820cf8
shubham rai <shubham.rai.5494360@gmail.com>
2022-06-27 07:56:11 +0530

    Calculating Daily Wage Of Employee

--- Employee_Wage_Problem.py
+++ Employee_Wage_Problem.py
@@ -1,14 +1,26 @@
 import random
 
 
-class checking_attendance:
-    def attendance(self):
-        empcheck = random.randint(0, 1)
-        if empcheck == 1:
-            print("Employee Is Present")
+class Daily_Wage:
+    def calculate_wage(self):
+        self.wage_per_hour = 20
+        self.is_full_time = 2
+        self.is_part_time = 1
+        self.emphrs = 0
+        self.empwage = 0
+
+        empcheck = random.randint(0, 2)
+        if empcheck == self.is_full_time:
+            print("Employee Is Working As Full-Time \n")
+            self.emphrs = 8
+            self.empwage = self.emphrs * self.wage_per_hour
+            print("Employee Daily Wage Is :", self.empwage)
         else:
-            print("Employee Is Absent")
+            print("Employee Is Working As Part-Time \n")
+            self.emphrs = 4
+            self.empwage = self.emphrs * self.wage_per_hour
+            print("Employee Daily Wage Is :", self.empwage)
 
 
-emp1 = checking_attendance()
-emp1.attendance()
+emp1 = Daily_Wage()
+emp1.calculate_wage()
