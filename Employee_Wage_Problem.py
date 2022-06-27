20ac7c3657a1f6c312192238de7d1df3f8a2d590
shubham rai <shubham.rai.5494360@gmail.com>
2022-06-27 08:00:28 +0530

    Calculating Employee Wage Till A Condition Reached To 20 Working Days And hours 100

--- Employee_Wage_Problem.py
+++ Employee_Wage_Problem.py
@@ -1,30 +1,36 @@
 import random
 
 
-class monthly_wages:
-    def calculate_monthly_wages(self):
-        self.day_per_month = 20
-        self.wage_per_hour = 20
-        self.emphrs = 0
-        self.empwage = 0
-        self.monthly_wage = 0
+class EmployeeWage_Problem:
+    def __init__(self):
+        self.num_of_working_days = 20
         self.is_full_time = 2
         self.is_part_time = 1
+        self.emp_rate_per_hour = 20
+        self.max_hrs_in_month = 100
 
-        empcheck = random.randint(0, 2)
-        if empcheck == self.is_full_time:
-            print("Employee Is Working As Full-Time  \n")
-            self.emphrs = 8
-            self.empwage = self.emphrs * self.wage_per_hour
-            self.monthly_wage = self.empwage * self.day_per_month
-            print("Monthly Wage Of Permanent Employee Is :", self.monthly_wage)
-        else:
-            print("Employee Is Working As Part-Time \n")
-            self.emphrs = 4
-            self.empwage = self.emphrs * self.wage_per_hour
-            self.monthly_wage = self.empwage * self.day_per_month
-            print("Monthly Wage Of Non Permanent Employee Is :", self.monthly_wage)
+    def total_working_hours(self):
+        totalemphrs = 0
+        totalworkingdays = 0
+        while totalemphrs < self.max_hrs_in_month and totalworkingdays < self.num_of_working_days:
+            totalworkingdays = totalworkingdays + 1
+            empcheck = random.randint(0, 2)
+            if empcheck == self.is_full_time:
+                emphrs = 8
+            elif empcheck == self.is_part_time:
+                emphrs = 4
+            else:
+                emphrs = 0
+
+            totalemphrs += emphrs
+            print("Days :", totalworkingdays)
+            print("EmpHrs: ", emphrs)
+
+        totalempwage = totalemphrs * self.emp_rate_per_hour
+        print("TotalEmployeeWage: ", totalempwage)
+        print("TotalWorkingDays :", totalworkingdays)
+        print("TotalEmpHrs: ", totalemphrs)
 
 
-emp1 = monthly_wages()
-emp1.calculate_monthly_wages()
+emp1 = EmployeeWage_Problem()
+emp1.total_working_hours()
